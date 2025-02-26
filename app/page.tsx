fc858006c9889bd236d9e9033c5c82746150dd2c
andreeabrindea <andreeabrindea61@gmail.com>
2025-02-27 01:18:15 +0200

    setup project add first section

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
-import Image from "next/image";
+"use client";
+import Image  from 'next/image';
+import profilePicture from '../public/me.jpg';
+import ThemeToggle from '../components/ui/theme-toggle';
+import { motion } from 'framer-motion';
+import { Button } from '../components/ui/button';
+import Link from 'next/link';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white trnasition-color duration-300">
+      <div className="mx-auto max-w-xl px-4 py-20">
+        <motion.header 
+          initial={{opacity: 0, x: -100}}
+          animate={{opacity: 1, x: 0}}
+          transition={{duration: 0.8}}
+          className="flex items-center justify-between mb-12">
+          <div className='w-20 h-20 rounded-full overflow-hidden'>
+            <Image src={profilePicture} alt='image with me' className='cursor-pointer transition-all duration-300 hover:scale-110'></Image>
+          </div>
+          <ThemeToggle/>
+        </motion.header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <main className='space-y-10'>
+          <motion.div
+           initial={{opacity: 0, x: 100}}
+           animate={{opacity: 1, x: 0}}
+           transition={{duration: 0.8}}
+           className='space-y-1'>
+            <h1 className='text-4xl font-bold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-600 bg-clip-text text-transparent inline-block'>
+                Andreea Brindea</h1>
+            <p className='text-gray-600 dark:gray-400'>Junior Software Engineer</p>
+            <p className='text-gray-600 dark:gray-400'>Cluj-Napoca, Romania</p>
+          </motion.div>
+          <motion.div
+          initial={{opacity: 0, x: 50}}
+          animate={{opacity: 1, x: 0}}
+          transition={{duration: 0.8}}
+          className='flex items-center gap-3'>
+            <Button className='rounded-full bg-gradient-to-r from-rose-600 to-indigo-600 text-white transition-transform hover:scale-105'>
+              Resume</Button>
+            <Link href="#" className='text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors'>
+              <FaGithub className='w-6 h-6'></FaGithub>
+            </Link>
+            <Link href="#" className='text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors'>
+              <FaLinkedin className='w-6 h-6'></FaLinkedin>
+            </Link>
+            <Link href="#" className='text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors'>
+              <BsFillTelephoneFill className='w-6 h-5'/>
+            </Link>
+            <Link href="#" className='text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors'>
+              <FaGithub className='w-6 h-6'></FaGithub>
+            </Link>
+          </motion.div>
+        </main>
+      </div>
     </div>
   );
 }
